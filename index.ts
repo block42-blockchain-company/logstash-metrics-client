af35cc054a153525e2cb8ff3c0a8c3e9a1b3c668
Florian Dulzky <florian.dulzky@block42.tech>
2021-05-11 14:26:26 +0200

    production mode

--- index.ts
+++ index.ts
@@ -3,24 +3,35 @@ import Axios from "axios"
 export default class LogstashMetrics {
 
     private static ls_url: string;
+    private static production: boolean;
 
     public static install(Vue: any, options: any = {
-        ls_url: ""
+        ls_url: "",
+        production: true
     }) {
-        console.log("inint logstash metrics")
         if (options["ls_url"] === undefined || options["ls_url"] == ""){
             console.log("Logstash URL (ls_url) is not specified in options");
         } else {
             this.ls_url = options["ls_url"];
+            this.production = options["production"];
         }
     }
 
     public static metric(type: string, message: string) {
-        if(this.ls_url !== undefined){
-            Axios.post(this.ls_url, {type: type, message: message}).then(r => {
-                console.log("send metrics to logstash")
+        if(this.ls_url !== undefined && this.production){
+            Axios.post(this.ls_url, this.buildMetric(type, message)).then(r => {
+                if (r.status != 200){
+                    console.log("!!! Could not send metrics to logstash !!!");
+                }
             });
+        } else {
+            console.log("logstash metric: ")
+            console.log(this.buildMetric(type, message));
         }
+    }
+
+    private static buildMetric(type: string, message: string){
+        return {type: type, message: message}
     }
 
 }
